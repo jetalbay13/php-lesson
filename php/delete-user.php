c9bf44893f7cdf5271a97c7ac846c7a7d5cfc1e3
jetalbay13 <159808680+jetalbay13@users.noreply.github.com>
2024-05-09 16:05:42 +0800

    Add files via upload

---
+++ php/delete-user.php
@@ -0,0 +1,18 @@
+<?php
+require('dbconfig.php');
+
+if(isset($_GET['id'])) {
+    $id = $_GET['id'];
+    
+    // Delete user from the database
+    $stmt = $conn->prepare("DELETE FROM users WHERE id = :id");
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+    
+    header("Location: ../index.php");
+    exit;
+} else {
+    echo "User ID not provided";
+    exit;
+}
+?>
